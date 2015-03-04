0ff05da9a8c9df41f9a1a5bcd5b32d58517faad2
Robin de Gier <de.gier.robin@gmail.com>
2015-03-04 16:51:10 +0100

    New glut, started new project.

---
+++ utils/ogl_headers.h
@@ -0,0 +1,12 @@
+#ifndef _OPENGL_HEADERS_
+#define _OPENGL_HEADERS_
+
+#include <stdlib.h>
+
+#ifdef __gnu_linux__
+    #include <GL/glut.h>
+#else
+    #include <gl/glut.h>
+#endif
+
+#endif // _OPENGL_HEADERS_
