e95b100c37325ee6dcb4b41228c169bf87392c00
Robin de Gier <de.gier.robin@gmail.com>
2015-03-08 15:12:04 +0100

    Added calculations for placing the mass object. Currently it uses placeholder structs placed in HammockDemo.h

--- ConsoleApplication1/HammockDemo.cpp
+++ ConsoleApplication1/HammockDemo.cpp
@@ -1,5 +1,29 @@
 #include "HammockDemo.h"
 
+cyclone::Vector3 Shape::GetMid(const cyclone::Vector3& from, const cyclone::Vector3& to) const
+{
+	//Cast 0.5 to real for compatibility with cyclone.
+	return from + (to - from) * (cyclone::real)0.5;
+}
+
+Line* Rect::GetCrossLines() const
+{
+	Line* toReturn = new Line[2];
+	toReturn[0] = Line(GetMid(p0, p3), GetMid(p1, p2));
+	toReturn[1] = Line(GetMid(p0, p1), GetMid(p2, p3));
+
+	return toReturn;
+}
+
+Line* Triangle::GetCrossLines() const
+{
+	Line* toReturn = new Line[2];
+	toReturn[0] = Line(p0, p1);
+	toReturn[1] = Line(GetMid(p0, p1), p2);
+
+	return toReturn;
+}
+
 const char* HammockDemo::getTitle()
 {
 	return "Assignment3, Hammock Demo";
@@ -29,4 +53,30 @@ void HammockDemo::key(unsigned char key)
     {
     case '1': printf("test");//currentShotType = PISTOL; break;
     }
+}
+
+void HammockDemo::SetMassPosition(const Shape& s)
+{
+	//Create a cross with the lines Parallel to the rect's lines.
+	//These lines will form the plane we will use to place the mass.
+	Line* lines = s.GetCrossLines();
+
+	//Use one of the plane's points as a reference point fo calculate the offset.
+	cyclone::Vector3 referencePoint(lines->start);
+	cyclone::Vector3 xzOffset(massRelativePos - cyclone::Vector3(referencePoint.x, 0, referencePoint.z));
+
+	//Now project the offset on the plane.
+	//To do so we first need the plane its normal. Every line perpendicular to
+	//the normal will be on the plane.
+	cyclone::Vector3 planeNormal(lines->GetLine().vectorProduct((++lines)->GetLine()));
+	planeNormal.normalise();
+
+	//Now calculate projection line x on plane V with normal n using projection = x - (x * n)n
+	cyclone::Vector3 xyzOffset(xzOffset - planeNormal * (xzOffset * planeNormal));
+
+	//Now set the mass its position.
+	massPos = referencePoint + xyzOffset;
+
+	//Lastly clean up the mess we made.
+	delete[] lines;
 }
--- ConsoleApplication1/HammockDemo.h
+++ ConsoleApplication1/HammockDemo.h
@@ -1,9 +1,54 @@
 #ifndef HAMMOCK_INCLUDED
 #define HAMMOCK_INCLUDED
 
+#include <cyclone/cyclone.h>
+
 #include "../utils/app.h"
 #include "../utils/timing.h"
 #include "../utils/ogl_headers.h"
+
+/** Simple line container. */
+struct Line
+{
+	Line(cyclone::Vector3 start = cyclone::Vector3(), cyclone::Vector3 end = cyclone::Vector3()) : start(start), end(end) { }
+
+	cyclone::Vector3 start;
+	cyclone::Vector3 end;
+
+	cyclone::Vector3 GetLine() { return end - start; }
+};
+
+/** Basic shape struct with member functions used for 
+calculating the position of the mass object. */
+struct Shape
+{
+	/** Gets the center coordinate of the line between vector to and from. */
+	cyclone::Vector3 GetMid(const cyclone::Vector3& from, const cyclone::Vector3& to) const;
+	/** Gets the cross lines from this shape. Needs shape specific implementation. */
+	virtual Line* GetCrossLines() const = 0;
+};
+
+/** Rect implementation of Shape. */
+struct Rect : public Shape
+{
+	cyclone::Vector3 p0;
+	cyclone::Vector3 p1;
+	cyclone::Vector3 p2;
+	cyclone::Vector3 p3;
+
+	/** Returns the lines parallel to p1-p0 and p2-p1. */
+	Line* GetCrossLines() const;
+};
+ /** Triangle implementation of Shape. */
+struct Triangle : public Shape
+{
+	cyclone::Vector3 p0;
+	cyclone::Vector3 p1;
+	cyclone::Vector3 p2;
+
+	/** Returns line p1-p0 and tline p2-(GetMid(p0,p1)). */
+	virtual Line* GetCrossLines() const;
+};
 
 class HammockDemo : public Application
 {
@@ -16,6 +61,12 @@ public:
 	virtual void key(unsigned char key);
 	/** Render the world */
 	virtual void render();
+	/** Calculates the mass object its position on the hammock. */
+	void SetMassPosition(const Shape&);
+
+private:
+	cyclone::Vector3 massRelativePos;
+	cyclone::Vector3 massPos;
 };
 
 
